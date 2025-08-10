5a0277db0fc78e8487871df26244d426b3933078
Vladimir Toivatrov <toivatrov18082000@gmail.com>
2025-08-10 10:36:32 +0300

    send form to mokky

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,94 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
-    const buttonAdd = document.getElementById("buttonAdd");
-
-// Тут у нас будут храниться задачи в виде JSON-массива
-// Ниже мы будем превращать JSON в HTML
-    let tasksJSON = [];
-
-// Укажите ваш ID проекта
+    const formElement = document.querySelector('.wedding-form');
+    const formSubmit = document.querySelector('.wedding-form__submit')
     const API_URL = "https://00de97822c6ccace.mokky.dev";
+    let formFields = {}
 
-// Получаем JSON-массив задач
-    async function fetchTasks() {
-        const resp = await fetch(`${API_URL}/users`);
-
-        if (resp.ok) {
-            const json = await resp.json();
-
-            return json;
-        } else {
-            alert("Не удалось получить задачи");
-            return [];
+    function addFormFields() {
+        const formData = new FormData(formElement)
+        formFields.name = formData.get('NAME')
+        formFields.surname = formData.get('SURNAME')
+        formFields.checkbox = {
+            checkbox_1: formData.get('check1'),
+            checkbox_2: formData.get('check2'),
+            checkbox_3: formData.get('check3'),
+            checkbox_4: formData.get('check4'),
+            checkbox_5: formData.get('check5'),
         }
+        formFields.radio = formData.get('radio')
+        formFields.comment = formData.get('COMMENT')
     }
 
-// Ренедерим JSON в HTML
-    function renderTasks() {
-        let html = "";
-
-        // Генерируем HTML-код для вставки в UL
-        tasksJSON.forEach((task) => {
-            const { id, text } = task;
-            html += `<li id="task-${id}">${text} <button onclick="removeTask(${task.id})">X</button></li>`;
-        });
-
-        taskList.innerHTML = html;
-    }
-
-// Отправляем на сервер задачу и отображаем на странице
-    async function addTask() {
-        const text = taskInput.value;
-
-        taskInput.value = "";
-
+    async function addUser() {
+        addFormFields()
         const resp = await fetch(`${API_URL}/users`, {
             method: "POST", // 👈 Отправляем именно POST-запрос
             headers: {
                 Accept: "application/json",
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ text }), // 👈 И передаем сам текст задачи
+            body: JSON.stringify(formFields), // 👈 И передаем сам текст задачи
         });
 
         if (resp.ok) {
-            const newTask = await resp.json();
-
-            // Сохраняем новую задачу в наш массив
-            tasksJSON.push(newTask);
-
-            // И сразу рендерим новый список
-            renderTasks();
+            console.log('Успешно отправлено');
+        } else {
+            console.log('Не удалось отправить');
         }
     }
 
-// Удаление задач
-    function removeTask(id) {
-        if (window.confirm("Удалить задачу?")) {
-            tasksJSON = tasksJSON.filter((task) => task.id !== id);
+    formElement.addEventListener('submit', function (e) {
+        e.preventDefault()
+        addUser()
+    })
 
-            // Не дожидаясь ответа от сервера, рендерим новый список уже без этой задачи
-            renderTasks();
-
-            // И отправляем запрос на удаление уже на сервере
-
-            fetch(`${API_URL}/users/${id}`, {
-                // 👈 Указываем ID задачи
-                method: "DELETE", // 👈 Отправляем DELETE-запрос
-            });
-        }
-    }
-
-    window.addEventListener("DOMContentLoaded", async () => {
-        // При первом рендере запращиваем список задач
-        tasksJSON = await fetchTasks();
-
-        // Отображаем задачи
-        renderTasks();
-
-        // Вещаем клик на добавление задач
-        buttonAdd.addEventListener("click", addTask);
-    });
 
 })
